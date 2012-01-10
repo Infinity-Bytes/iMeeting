46552ca8332dbc74178a57f22e67150ab3c5e315
Jesus Cagide Alvarado <set311@gmail.com>
2012-01-10 13:58:58 -0600

    Initial Commit

---
+++ iMeeting/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iMeeting
+//
+//  Created by Jesus Cagide on 10/01/12.
+//  Copyright (c) 2012 INEGI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
