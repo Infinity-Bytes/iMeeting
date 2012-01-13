9307a6d884a85a97aeddfbb9f1c413567f1e9864
Cagide <set311@gmail.com>
2012-01-12 18:19:04 -0600

    agregando lista de personas anidadas

---
+++ iMeeting/ControladorListaPersonas.h
@@ -0,0 +1,18 @@
+//
+//  ControladorListaPersonas.h
+//  iMeeting
+//
+//  Created by Jesus Cagide on 12/01/12.
+//  Copyright (c) 2012 INEGI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ControladorListaPersonas : UIViewController<UITableViewDelegate, UITableViewDataSource>
+
+
+
+@property(nonatomic, retain) NSArray *datos;
+@property(nonatomic, assign) IBOutlet  UITableView *tablaDatos;
+
+@end
